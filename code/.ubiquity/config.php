b5d3b0f364f6e1964256effa709be22564fcba04
Isaac <isaac.kuang@gmail.com>
2019-07-25 13:43:21 +0800

    first commit

---
+++ code/.ubiquity/config.php
@@ -0,0 +1,2 @@
+<?php
+return array("port"=>80,"sessionName"=>"s5d381f0259e65","host"=>"0.0.0.0");
---
+++ code/app/controllers/DefaultController.php
@@ -0,0 +1,35 @@
+<?php
+namespace controllers;
+
+ /**
+  * Controller DefaultController
+  * @route("/hello","automated"=>true)
+  **/
+class DefaultController extends ControllerBase{
+
+	public function index()
+	{
+		echo 'Hello world!';
+	}
+		
+
+	/**
+	 *@route("to/{name}/")
+	**/
+	public function sayHello($name)
+	{
+		echo 'Hello!!!! ' . $name . '!';
+	}
+
+
+	/**
+	 *@route("info/{title}/{message}")
+	**/
+	public function information($title,$message=nothing)
+	{
+		
+		$this->loadView('DefaultController/information.html',compact('title','message'));
+
+	}
+
+}
